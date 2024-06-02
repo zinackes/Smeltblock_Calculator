a71133a9f209d3999583555a458b393fefd2ca50
zinackes <zinacke@gmail.com>
2024-06-02 16:31:07 +0300

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -17,3 +17,24 @@ boxs.forEach(function (box) {
         target.classList.add("group_content_active");
     })
 })
+
+
+let group_boxs = document.querySelectorAll(".group_content .box")
+
+group_boxs.forEach(function (box) {
+    box.addEventListener("click", function () {
+        let index = Array.from(group_boxs).indexOf(this);
+
+        for(let i = 0; i < boxs.length; i++){
+            group_boxs[i].classList.remove("selected");
+        }        
+        box.classList.add("selected");
+        document.querySelectorAll(".window_active").forEach(function (onglet){
+            onglet.classList.remove("window_active");
+        })
+
+        let target = document.querySelectorAll(".window")[index];
+        console.log(target);
+        target.classList.add("window_active");
+    })
+})
