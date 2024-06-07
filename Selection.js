641eb13d4669bb631fab29a5ae9cf5f5f63d2f41
zinackes <zinacke@gmail.com>
2024-06-07 18:13:05 +0300

    Fix

--- Selection.js
+++ Selection.js
@@ -1,9 +1,9 @@
-function Choice(Item, Cequonveut){
+function Choice(Id){
+    IdObjectif = Id;
+    console.log(IdObjectif);
 }
 
-
-
-let content = document.querySelectorAll(".content");
+let content = document.querySelectorAll(".item .content");
 
 content.forEach(function (selectionne) {
     selectionne.addEventListener("click", function () {
@@ -22,3 +22,34 @@ content.forEach(function (selectionne) {
         }
     })
 })
+
+let effets = document.querySelectorAll(".effets .sub_content button");
+
+effets.forEach(function (selectionne) {
+    selectionne.addEventListener("click", function () {
+        let index = Array.from(effets).indexOf(this);
+        selectionne.classList.toggle("selected");
+        Effets[index] = EffetsNewNumber[index];
+        console.log(Effets[index]);
+    })
+});
+
+
+function InputConvertion(){
+    let input = document.querySelectorAll("#reglages input");
+    Inputs = [];
+    for(i = 0; i < input.length; i++){
+        Inputs.push(input[i].value);
+    }
+}
+
+function ReturnInputs(){
+    let input = document.querySelectorAll("#reglages input");
+    let inputP = document.querySelectorAll("#reglages p");
+    for(let i = 0; i < input.length; i++){
+        input[i].value = Inputs[i];
+        inputP[i].textContent = Inputs[i];
+    }
+}
+
+setInterval(InputConvertion, 10000);
