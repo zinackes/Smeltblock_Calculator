212a9e609893d6cf3c8f97968e537d887277e509
zinackes <zinacke@gmail.com>
2024-06-07 18:12:32 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,26 @@
+let Fermier_Fp_Gain = [0, 1];
+
+let Fermier = [Fermier_Fp_Gain];
+
+let Jobs = [Fermier];
+
+let IdObjectif = 0;
+
+
+let Speed1 = 1;
+let Speed2 = 1;
+let SpeedBeac1 = 1;
+let SpeedBeac2 = 1;
+let Haste1 = 1;
+let Haste2 = 1;
+let Force1 = 1;
+let Force2 = 1;
+let FlySpeed1 = 1;
+let FlySpeed2 = 1;
+let FlySpeed3 = 1;
+
+
+let EffetsNewNumber = [1.34, 1.34, 1.34, 1.34, 1.34, 1.34, 1.34, 1.34, 1.34, 1.34,1.34, 1.34];
+let Effets = [Speed1, Speed2];
+
+let Inputs = [];
